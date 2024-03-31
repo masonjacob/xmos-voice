d39397820b2713832b7cfadee6e5b744e353b344
Derek R. <derek_raffetto@uri.edu>
2024-03-31 13:35:17 -0400

    Working parrallel control with techincal debt mostly paid. Delay function for step generation still doesn't work properly

--- src/examples/TMCStepper_SPI/main.cpp
+++ src/examples/TMCStepper_SPI/main.cpp
@@ -25,16 +25,24 @@ uint32_t INITIATE_ACTION = 0xFEEDF00D;
 uint32_t DONE_STATUS = 0xD04ED04E;
 uint32_t STALLED = 0xBAAAAAAD;
 uint32_t SHUTDOWN = 0xEC11EC11;
+uint32_t WAITING = 0x00000000;
+uint32_t GENERATINGWAVE = 0xA1A1A1A1;
 
-
-// port_t p_miso  = XS1_PORT_1P;
-// port_t p_ss = XS1_PORT_1A; //Active Low CHIP SELECT
-// port_t p_sclk = XS1_PORT_1C; //SPI CLK
-// port_t p_mosi = XS1_PORT_1D;
-// xclock_t cb = XS1_CLKBLK_1;
-// port_t led = XS1_PORT_4F;
-// port_t step_pin = XS1_PORT_1O; //X0D38, I2C_SDA, pin 3 on expansion header
 bool stallDetected = false;
+bool shutdown = false;
+
+// port_t SHOULDER_SWIVEL_STEP_PIN = chan_alloc(); //Spins in theta, that is 360 degrees parrallel plane to the ground
+// port_t SHOULDER_JOINT_STEP_PIN = chan_alloc(); //Moves in the phi, that is up and down
+// port_t ELBOW_JOINT_STEP_PIN = chan_alloc();
+port_t WRIST_SWIVEL_STEP_PIN = XS1_PORT_1N; //X0D37 I2C_SCL, pin 5 on exapnsion header
+port_t WRIST_JOINT_STEP_PIN = XS1_PORT_1O; //X0D38, I2C_SDA, pin 3 on expansion header
+
+port_t p_miso  = XS1_PORT_1P;
+port_t p_ss = XS1_PORT_4A; //Active Low CHIP SELECT
+port_t p_sclk = XS1_PORT_1C; //SPI CLK
+port_t p_mosi = XS1_PORT_1D;
+xclock_t cb = XS1_CLKBLK_1;
+port_t led = XS1_PORT_4F;
 
 //"XS1_PORT_1D"  Name="PORT_SPI_MOSI"/>
 //<Port Location="XS1_PORT_1P"  Name="PORT_SPI_MISO"/>
@@ -99,102 +107,9 @@ void setup_driver(TMC5160Stepper* driver, int current) {
 
 }
 
-// class SquareWave {
-// public:
-//     port_t step_pin;
-//     int delay_in_microseconds;
-//     int numSteps;
-//     int currentStep;
-//     bool currentState;
-//     bool enabled;
-//     unsigned long lastToggleTime; // This now holds the starting reference time
-//     hwtimer_t timer;
-
-//     // Modified constructor to accept an initial starting time
-//     SquareWave(port_t pin, int delayMicros, int steps, hwtimer_t hwTimer, int startTime)
-//         : step_pin(pin), delay_in_microseconds(delayMicros), numSteps(steps), timer(hwTimer),
-//           currentStep(0), currentState(false), enabled(true), lastToggleTime(startTime) {}
-
-//     void update() {
-//         if (enabled) {
-//             int currentTime = hwtimer_get_time(timer);
-//             if ((currentTime - lastToggleTime) >= delay_in_microseconds * 100) { //For some reason it seems the timer is in 10^-8, so micro, 10^-6 to 10^-8, micro * 10^2 = 10^-8 version
-//                 std::cout << ((currentTime -lastToggleTime) * 100 )<< std::endl;
-//                 currentState = !currentState;
-//                 port_out(step_pin, currentState);
-//                 std::cout << currentState << std::endl;
-//                 lastToggleTime = currentTime; // Update with the current time for next toggle
-                
-//                 if(currentState) //Our steps are number of high's so only add if we get a high
-//                     currentStep++;
-
-//                 if (currentStep >= numSteps) {
-//                     enabled = false;
-//                     currentStep = 0;
-//                     port_out(step_pin, 0); // Optionally set the pin to a known state (low) when disabled
-//                 }
-//             }
-//         }
-//     }
-
-//     // Method to re-enable the square wave with a new starting time
-//     void reEnable(int steps, unsigned long startTime) {
-//         numSteps = steps;
-//         currentStep = 0;
-//         enabled = true;
-//         lastToggleTime = startTime; // Set the lastToggleTime to the new starting time
-//     }
-// };
-
-// // Assume global or member variable for storage
-// std::vector<SquareWave> squareWaves;
-
-// // ISR remains largely the same in structure, iterating over objects
-// void ISR() {
-//     for (auto& squareWave : squareWaves) {
-//         squareWave.update();
-//     }
-// }
-
-// class StepWaveGenerator {
-// public:
-//     StepWaveGenerator(port_t pin, int count, int delayUs)
-//     : stepPin(pin) {}
-
-//     void generateWave(port_t stepPin, uint32_t stepCount, uint32_t stepDelayMicroseconds) 
-//     {        
-        
-//         for (int i = 0; i < stepCount; ++i) 
-//         {
-//             // if(stallDetected) //Global variable. Since PARJOB completes executions before returning control to after the PARJOB {}, we need a way to from outside the single control line, stop the motors in case of a stall
-//             //     break;
-//             // port_out(stepPin, 1);
-//             // delay_microseconds(stepDelayMicroseconds);
-//             // port_out(stepPin, 0);
-//             // delay_microseconds(stepDelayMicroseconds);
-//         }
-//         //These will keep getting run over and over in the PAR, so we want stepCount to be 0 until updated
-//         stepCount = 0;
-//         // port_out(stepPin, 0); //Not needed but just in case
-        
-//     }
-
-//     // // Optionally, a method to update parameters if neededS
-//     // void updateParameters(int count, int delayUs) {
-//     //     stepCount = count;
-//     //     stepDelayMicroseconds = delayUs;
-//     // }
-
-// private:
-//     port_t stepPin;
-// };
-
-DECLARE_JOB(myGenerateWave, (port_t, uint32_t, uint32_t, chanend_t));
+// DECLARE_JOB(myGenerateWave, (port_t, uint32_t, uint32_t, chanend_t));
 void myGenerateWave(port_t stepPin, uint32_t stepCount, uint32_t stepDelayMicroseconds, chanend_t controlToStepper) 
 {        
-
-    chan_in_word(controlToStepper); // Weirdly works and is fine. Putting below in the loop causes issues though
-
     for (int i = 0; i < stepCount; ++i) 
     {
         port_out(stepPin, 1);
@@ -204,15 +119,14 @@ void myGenerateWave(port_t stepPin, uint32_t stepCount, uint32_t stepDelayMicros
         // delay_microseconds(stepDelayMicroseconds);
         printf("\ndelay disabled %d\n", (int) stepPin);
 
-    //FIX ME! Weird issue when trying to use the channel down here.
-        // if(chan_in_word(controlToStepper) == STALLED)
-        // {
-        //     stepCount = 0;
-        //     port_out(stepPin, 0);
-        //     return;
-        // }
+        chan_out_word(controlToStepper, GENERATINGWAVE);
 
-
+        if(stallDetected)
+        {
+            stepCount = 0;
+            port_out(stepPin, 0);
+            return;
+        }
     }
 
     //These will keep getting run over and over in the PAR, so we want stepCount to be 0 until updated
@@ -225,7 +139,7 @@ void myGenerateWave(port_t stepPin, uint32_t stepCount, uint32_t stepDelayMicros
 DECLARE_JOB(parCallGenerateWave, (port_t, chanend_t));  
 void parCallGenerateWave(port_t pin, chanend_t controlToStepper)
 {
-    chan_out_word(controlToStepper,DONE_STATUS);
+    chan_out_word(controlToStepper,DONE_STATUS); //Initialized so control knows generators are ready to initiate
     uint32_t control=0;
     uint32_t stepCount=0;
     uint32_t stepDelayMicroseconds=0;
@@ -236,18 +150,20 @@ void parCallGenerateWave(port_t pin, chanend_t controlToStepper)
         if(control == SHUTDOWN) //Our shutdown needs to available before and after the step wave is generated.
             return;
         
-        
        if(control == INITIATE_ACTION)
         {
             stepCount = chan_in_word(controlToStepper);
             stepDelayMicroseconds = chan_in_word(controlToStepper);
 
-                myGenerateWave(pin, stepCount, stepDelayMicroseconds, controlToStepper);
+            myGenerateWave(pin, stepCount, stepDelayMicroseconds, controlToStepper);
 
-            if(chan_in_word(controlToStepper) == SHUTDOWN)
+            if(shutdown)
                 return;
             else
+            {
+                printf("DONE");
                 chan_out_word(controlToStepper, DONE_STATUS);
+            }
         }
     }
     return;
@@ -256,20 +172,29 @@ void parCallGenerateWave(port_t pin, chanend_t controlToStepper)
 DECLARE_JOB(parControlLoop, (chanend_t,chanend_t));
 void parControlLoop(chanend_t stepper0, chanend_t stepper1)
 {
-    for(int i=0; i<1; i++)
+    // for(int i=0; i<1; i++)
+    // {
+
+    while(1)
     {
 
         //This weirdly works with no issue
         // printf("Before");
         // delay_microseconds(100);
         // printf("After");
+
+        printf("Top of control");
+        
+
         
         if(chan_in_word(stepper0) == DONE_STATUS)
         {
             chan_out_word(stepper0, INITIATE_ACTION);   
             chan_out_word(stepper0, 20);
             chan_out_word(stepper0, 100);
+
         }
+       
 
         if(chan_in_word(stepper1) == DONE_STATUS)
         {
@@ -277,11 +202,24 @@ void parControlLoop(chanend_t stepper0, chanend_t stepper1)
             chan_out_word(stepper1, 15);
             chan_out_word(stepper1, 50);
         }
+    
+
+        // printf("Before waiting i ", i);
+        // while(chan_in_word(stepper0) != DONE_STATUS || chan_in_word(stepper1) != DONE_STATUS)
+        //     {
+        //         chan_out_word(stepper0, WAITING);
+        //         chan_out_word(stepper1, WAITING);
+        //     }
+
+
+        // if(i == 10)
+        //     stallDetected = true;
+
+        // i++;
     }
 
-    chan_out_word(stepper0, SHUTDOWN);
-    printf("here");
-    chan_out_word(stepper1, SHUTDOWN);
+    // chan_out_word(stepper0, SHUTDOWN);
+    // chan_out_word(stepper1, SHUTDOWN);
 
     return;
 }
@@ -319,38 +257,38 @@ int main()
     // int start = hwtimer_get_time(timer);
             //       SecMseMicNan
 
-    // std::vector<StepWaveGenerator> generators;
     
     int stepCount = 10;
     int stepDelay = 250; //In microseconds
-
-    port_t pin2 = XS1_PORT_1N;
-    port_enable(pin2);
-    port_t step_pin = XS1_PORT_1O;
-    port_enable(step_pin);
-
-    // generators.push_back(StepWaveGenerator(step_pin, stepCount, stepDelay));
-    // generators.push_back(StepWaveGenerator(pin2, 20, 100));
-
-    // StepWaveGenerator gen = StepWaveGenerator(pin2, 20, 100);
-    // StepWaveGenerator gen2 = StepWaveGenerator(step_pin, stepCount, stepDelay);
     
-    channel_t stepper_motors[2];
-    stepper_motors[0] = chan_alloc();
-    stepper_motors[1] = chan_alloc();
+    // port_enable(SHOULDER_SWIVEL_STEP_PIN);
+    // port_enable(SHOULDER_JOINT_STEP_PIN);
+    // port_enable(ELBOW_JOINT_STEP_PIN);
+    port_enable(WRIST_SWIVEL_STEP_PIN);
+    port_enable(WRIST_JOINT_STEP_PIN);
+
+/*NOTICE! When using chan ends, the core blocks until it gets something on the channel. 
+Saying chan_in_word(channelA) will force the core to sit there until a different core talks back*/
+
+    channel_t SHOULDER_SWIVEL_CHANNEL = chan_alloc(); //Spins in theta, that is 360 degrees parrallel plane to the ground
+    channel_t SHOULDER_JOINT_CHANNEL = chan_alloc(); //Moves in the phi, that is up and down
+    channel_t ELBOW_JOINT_CHANNEL = chan_alloc();
+    channel_t WRIST_SWIVEL_CHANNEL = chan_alloc();//Spins in theta, that is 360 degrees parrallel plane to the ground
+    channel_t WRIST_JOINT_CHANNEL = chan_alloc(); //Moves in the phi, that is up and down
     
     PAR_JOBS( //Step pin wave generators
-        PJOB(parControlLoop, (stepper_motors[0].end_a, stepper_motors[1].end_a)),
-        PJOB(parCallGenerateWave, (pin2, stepper_motors[0].end_b)), //make sure to seperate PJOB by a ","
-        PJOB(parCallGenerateWave, (step_pin, stepper_motors[1].end_b))
+        PJOB(parControlLoop, (SHOULDER_SWIVEL_CHANNEL.end_a, SHOULDER_JOINT_CHANNEL.end_a)),
+        PJOB(parCallGenerateWave, (WRIST_SWIVEL_STEP_PIN, SHOULDER_SWIVEL_CHANNEL.end_b)), //make sure to seperate PJOB by a ","
+        PJOB(parCallGenerateWave, (WRIST_JOINT_STEP_PIN, SHOULDER_JOINT_CHANNEL.end_b))
 
 
     );
 
-    chan_free(stepper_motors[0]);
-    chan_free(stepper_motors[1]);
-
-
+    chan_free(SHOULDER_SWIVEL_CHANNEL);
+    chan_free(SHOULDER_JOINT_CHANNEL);
+    chan_free(ELBOW_JOINT_CHANNEL);
+    chan_free(WRIST_SWIVEL_CHANNEL);
+    chan_free(WRIST_JOINT_CHANNEL);
 
 
        
